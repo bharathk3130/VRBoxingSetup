9a8d3bd585e304c8cb0ea98547180ee5d322c8cc
Clickbait <48381062+bharathk3130@users.noreply.github.com>
2024-10-13 03:46:07 +0530

    Small changes

---
+++ Assets/_Project/Scripts/StarterScripts/PunchingCubeDeathEffects.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(DissolveEffect))]
+public class PunchingCubeDeathEffects : MonoBehaviour
+{
+    [SerializeField] ParticleSystem _deathEffectParticles;
+    [SerializeField] AudioSource _deathAudio;
+    DissolveEffect _dissolveEffect;
+
+    void Awake()
+    {
+        _dissolveEffect = GetComponent<DissolveEffect>();
+    }
+
+    public void Play()
+    {
+        _dissolveEffect.Play(DestroySelf);
+        _deathAudio.Play();
+        _deathEffectParticles.Play();
+    }
+    
+    void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
